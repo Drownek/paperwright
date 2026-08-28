c7501fd61240c425aa5ec509291ecc770940d774
Drownek <piotrgamingyt@gmail.com>
2026-08-28 19:12:53 +0200

    ci: fix java 26 toolchain resolution and revert daemon version

--- example_plugin/settings.gradle.kts
+++ example_plugin/settings.gradle.kts
@@ -5,6 +5,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 includeBuild("../gradle-plugin")
 
 rootProject.name = "example-plugin"
